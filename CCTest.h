f13a1ed2baf776821669d7bee03b9c146ae318d7
Cristian Rivera <cristianrivera@macbook-pro.home>
2014-09-02 01:22:16 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Cristian Rivera on 9/2/14.
+//  Copyright (c) 2014 DTS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Cristian Rivera on 9/2/14.
+//  Copyright (c) 2014 DTS. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
