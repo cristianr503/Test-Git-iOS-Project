f6a19864db7c4673370a6ba814c0efa71099bc05
Cristian Rivera <cristianrivera@macbook-pro.home>
2014-09-02 01:07:20 -0400

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Cristian Rivera on 9/2/14.
+//  Copyright (c) 2014 DTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Cristian Rivera on 9/2/14.
+//  Copyright (c) 2014 DTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
